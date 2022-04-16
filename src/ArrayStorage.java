bc8e567a14a8f317c36d029918151d18693659d4
maxburdygin <burdigin.maks@gmail.com>
2022-04-16 15:10:53 +0700

    1. Array methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,47 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int length = size();
+        for (int i = 0; i < length; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (r != null && get(r.uuid) == null)
+            storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size());
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null)
+                return i;
+        }
+        return 10000;
     }
 }
